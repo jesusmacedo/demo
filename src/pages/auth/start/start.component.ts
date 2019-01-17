d7a198f4c6f69c0d26fc8df1d2860c07a9916965
Jesus Macedo <macedo.rjpablo@gmail.com>
2019-01-16 23:22:35 -0600

    Added form to create a new account.

--- src/pages/auth/start/start.component.ts
+++ src/pages/auth/start/start.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { SpinnerVisibilityService } from 'ng-http-loader';
+import { EAlert } from '../../../enums/common/alert.enum';
+import { IAlert } from '../../../interfaces/common/alert.interface';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
     selector: 'demo-start',
@@ -10,12 +15,31 @@ export class StartComponent implements OnInit {
     // available languages
     EN = 'en';
     ES = 'es';
+    // handle account creation
+    showForm = false;
+    creationForm: FormGroup;
+    resultAlert: IAlert;
 
-    constructor(private translate: TranslateService) {}
+    constructor(
+        private translate: TranslateService,
+        private fb: FormBuilder,
+        private spinner: SpinnerVisibilityService,
+        private authService: AuthService
+    ) {}
 
     // * Angular Lifecycle
 
-    ngOnInit() {}
+    /**
+     * Initialize the `FormGroup` to create a new account.
+     */
+    ngOnInit() {
+        this.creationForm = this.fb.group({
+            email: new FormControl('', Validators.compose([Validators.email, Validators.required])),
+            firstName: new FormControl('', Validators.compose([Validators.required])),
+            lastName: new FormControl('', Validators.compose([Validators.required])),
+            password: new FormControl('', Validators.compose([Validators.required]))
+        });
+    }
 
     // * User Interaction
 
@@ -25,5 +49,36 @@ export class StartComponent implements OnInit {
      */
     didSelectLanguage(lang: 'es' | 'en'): void {
         this.translate.use(lang);
+    }
+
+    /**
+     * Take the data from `creationForm` and create a new `ICreateAccount`.
+     */
+    didPressSubmit(): void {
+        this.resultAlert = undefined;
+        this.spinner.show();
+
+        const newAccount: ICreateAccount = {
+            firstname: this.creationForm.controls['firstName'].value,
+            lastname: this.creationForm.controls['lastName'].value,
+            email: this.creationForm.controls['email'].value,
+            password: this.creationForm.controls['password'].value
+        };
+
+        this.authService.postCreate(newAccount).subscribe(
+            (response: { success: string }) => {
+                this.resultAlert = {
+                    content: response.success,
+                    type: EAlert.SUCCESS
+                };
+            },
+            (e: any) => {
+                this.resultAlert = {
+                    content: e.error.message,
+                    type: EAlert.ERROR
+                };
+            },
+            () => this.spinner.hide()
+        );
     }
 }

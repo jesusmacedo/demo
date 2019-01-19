b7320fbf8bbb1173272cab9774295d05bd972821
Jesus Macedo <macedo.rjpablo@gmail.com>
2019-01-18 22:39:36 -0600

    Improved header and created request account component.

---
+++ src/pages/accounts/request/request.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'demo-request',
+    templateUrl: './request.component.html',
+    styleUrls: ['./request.component.sass']
+})
+export class RequestComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}

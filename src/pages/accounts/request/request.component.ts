1f8167d387b39bf5c3df1e387c09f824df4e6aa5
Jesus Macedo <macedo.rjpablo@gmail.com>
2019-01-19 00:02:57 -0600

    Finished request form.

--- src/pages/accounts/request/request.component.ts
+++ src/pages/accounts/request/request.component.ts
@@ -1,12 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ICard } from '../../../interfaces/accounts/card.interface';
+import { AccountsService } from '../../../services/accounts/accounts.service';
 
 @Component({
     selector: 'demo-request',
     templateUrl: './request.component.html',
     styleUrls: ['./request.component.sass']
 })
-export class RequestComponent implements OnInit {
-    constructor() {}
+export class RequestComponent implements OnInit, OnDestroy {
+    // handle card selection
+    cardCatalog: ICard[] = [];
+    selectedCard: ICard;
+    // handle observable subscriptions
+    private _getCardCatalogSubscription: Subscription;
+    private _postAccounts: Subscription;
 
-    ngOnInit() {}
+    constructor(private accountsService: AccountsService) {}
+
+    // * Angular Lifecycle
+
+    /**
+     * Get all available `ICard` to choose from.
+     */
+    ngOnInit() {
+        this._getCardCatalogSubscription = this.accountsService.getCardCatalog().subscribe((cards: ICard[]) => {
+            this.cardCatalog = cards;
+        });
+    }
+
+    /**
+     * Unsubscribe from `Observable`.
+     */
+    ngOnDestroy() {
+        this._getCardCatalogSubscription.unsubscribe();
+        this._postAccounts.unsubscribe();
+    }
+
+    // * User Interaction
+
+    /**
+     * Set the selected `ICard` by the received ID.
+     * @param card from dropdown.
+     */
+    didSelectCard(card: number): void {
+        this.selectedCard = this.cardCatalog[card];
+    }
+
+    /**
+     * Send card request.
+     */
+    didPressSend(): void {
+        this._postAccounts = this.accountsService.postAccounts(this.selectedCard).subscribe(() => {});
+    }
 }
--- src/services/accounts/accounts.service.ts
+++ src/services/accounts/accounts.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { EApi } from '../../enums/api.enum';
+import { ECardType } from '../../enums/common/card-type.enum';
+import { ICard } from '../../interfaces/accounts/card.interface';
+import { MSession } from '../../models/auth/session.model';
 
 /**
  * `service` for all accounts-related endpoints.
@@ -18,5 +21,34 @@ export class AccountsService {
      */
     getAccounts(): Observable<IAccount[]> {
         return this.http.get(EApi.getAccounts).pipe(map((accounts: { response: IAccount[] }) => accounts.response));
+    }
+
+    /**
+     * Get all available `ICard` to choose from.
+     */
+    getCardCatalog(): Observable<ICard[]> {
+        return this.http
+            .get(EApi.getCardCatalog)
+            .pipe(map((catalog: { response: { type_cards: ICard[] } }) => catalog.response.type_cards));
+    }
+
+    // * Public POST Endpoints
+
+    /**
+     * Request the selected `ICard`.
+     * @param card selected by user.
+     */
+    postAccounts(card: ICard): Observable<any> {
+        const payload: {
+            userId: string;
+            type: ECardType;
+            name: string;
+        } = {
+            userId: MSession.getSessionData().id,
+            type: card.type,
+            name: card.name
+        };
+
+        return this.http.post(EApi.postAccount, payload).pipe(tap(res => console.log(res)));
     }
 }
